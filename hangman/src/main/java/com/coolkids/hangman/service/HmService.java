eda6cd15f970a059b5a1138d46f96f4542fe27b6
Richard Ching <richardcching@gmail.com>
2021-02-04 11:02:53 -0500

    Update HmService.java

--- hangman/src/main/java/com/coolkids/hangman/service/HmService.java
+++ hangman/src/main/java/com/coolkids/hangman/service/HmService.java
@@ -11,35 +11,9 @@ import java.util.Random;
 @Service
 public class HmService implements HmServiceInterface {
 
-    private final GamesDao gDao;
-    private final RoundsDao rDao;
-
-    public HangmanService(GamesDao gDao, RoundsDao rDao) {
-        this.gDao = gDao;
-        this.rDao = rDao;
-    }
-
     @Override
     public Game createGame(Game game) {
-
-        // Set a random answer for new game
-        Random rand = new Random();
-        String answer = "";
-        while (answer.length() < 4){
-            String randInt = String.valueOf(rand.nextInt(9));
-            if (answer.indexOf(randInt) == -1){
-                answer = answer + randInt;
-            }
-        }
-        game.setAnswer(answer);
-
-        // Add game to storage
-        gDao.add(game);
-
-        // set the answer to null so user cannot see it.
-        game.setAnswer(null);
-
-        return game;
+        return null;
     }
 
     @Override
