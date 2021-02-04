f1a9a7dcfaac49b7a0debe464951792bb21d1768
joeyharbert <joey.harbert@gmail.com>
2021-02-04 08:28:32 -0600

    spring initialzr

---
+++ hangman/src/main/java/com/coolkids/hangman/HangmanApplication.java
@@ -0,0 +1,13 @@
+package com.coolkids.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanApplication.class, args);
+	}
+
+}
